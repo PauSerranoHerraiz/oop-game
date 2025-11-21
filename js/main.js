6d017e06dba6953b7216efaec69a25dae4fd649f
Pau Serrano <pauserrano14@gmail.com>
2025-11-21 11:24:29 +0100

    feat: implement player movement

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-console.log ("Hello World")
+
+class Player {
+    constructor() {
+        this.width = 20;
+        this.height = 10;
+        this.positionX = 50;
+        this.positionY = 0;
+
+        this.updateUi()
+    }
+    updateUi() {
+        const playerElm = document.getElementById("player")
+        playerElm.style.width = this.width + "vw"
+        playerElm.style.height = this.height + "vh"
+        playerElm.style.left = this.positionX + "vw"
+        playerElm.style.bottom = this.positionY + "vh"
+
+    }
+    moveLeft() {
+        if (this.positionX > 0) {
+            this.positionX--;
+            this.updateUi()
+        }
+    }
+    moveRight() {
+        if (this.positionX < 100 - this.width){
+        this.positionX++;
+        this.updateUi();
+        }
+    }
+}
+
+const player = new Player() //crea una instancia de la clase Player
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'ArrowLeft') {
+        player.moveLeft()
+    } else if (e.code === 'ArrowRight') {
+        player.moveRight()
+    }
+});
